ab21c8090845ec517bbb72350026ef7b2a37b8c3
om-3004 <omthakkar3004@gmail.com>
2024-02-17 10:18:38 +0530

    Made LinkedList generic using template

--- Assignment-3/include/linkedlist.h
+++ Assignment-3/include/linkedlist.h
@@ -2,32 +2,202 @@
 #define _LINKEDLIST_H
 #include <iostream>
 
+template <typename T>
 struct Node {
-	int m_Data{ 0 };
-	std::shared_ptr<Node> m_Next{ nullptr };
+	T m_Data;
+	std::shared_ptr<Node<T>> m_Next;
 
-	Node() : m_Data{ 0 }, m_Next{ nullptr } {}
-	Node(int data) : m_Data{ data }, m_Next{ nullptr } {}
+	Node(T data) : m_Data{ data }, m_Next{ nullptr } {}
 };
 
+template <typename T>
 class LinkedList {
-	std::shared_ptr<Node> head;
+	std::shared_ptr<Node<T>> head;
 
 public:
 	LinkedList() : head{ nullptr } {}
-	LinkedList(const std::initializer_list<int>&);
+	LinkedList(const std::initializer_list<T>&);
 
-	void insertNodeStart(int);
-	void insertNodeEnd(int);
-	void insertNodePos(unsigned int, int );
+	void insertNodeStart(T data);
+	void insertNodeEnd(T data);
+	void insertNodePos(unsigned int pos, T data );
 	int getLength() const ;
 
 	void deleteNodeStart();
 	void deleteNodeEnd();
-	void deleteNodePos(unsigned int);
+	void deleteNodePos(unsigned int pos);
 
 	void displayList();
 };
 
+//constructor taking initializer list as arguemnt
+template <typename T>
+LinkedList<T>::LinkedList(const std::initializer_list<T>& list) {
+	for (auto& val : list) {
+		insertNodeEnd(val);
+	}
+}
+
+template <typename T>
+void LinkedList<T>::insertNodeStart(T data) {
+	if (head == nullptr) {
+		head = std::make_shared<Node<T>>(data);
+		return;
+	}
+	auto newNode = std::make_shared<Node<T>>(data);
+	newNode->m_Next = head;
+	head = newNode;
+}
+
+template <typename T>
+void LinkedList<T>::insertNodeEnd(T data) {
+
+	if (head == nullptr) {
+		head = std::make_shared<Node<T>>(data);
+		return;
+	}
+	auto newNode = std::make_shared<Node<T>>(data);
+	auto temp = head;
+
+	while (temp->m_Next != nullptr) {
+		temp = temp->m_Next;
+	}
+	temp->m_Next = newNode;
+}
+
+template <typename T>
+void LinkedList<T>::insertNodePos(unsigned int pos, T data) {
+	if (pos == 1) {
+		insertNodeStart(data);
+		return;
+	}
+	if (pos == getLength() + 1) {
+		insertNodeEnd(data);
+		return;
+	}
+	if (pos > getLength()) {
+		std::cout << "Can't insert. Position is larger than size" << '\n';
+		return;
+	}
+
+	auto temp = head;
+	auto cnt{ 1 };
+	while (cnt != pos - 1) {
+		temp = temp->m_Next;
+		cnt++;
+	}
+
+	auto newNode = std::make_shared<Node<T>>(data);
+	newNode->m_Next = temp->m_Next;
+	temp->m_Next = newNode;
+}
+
+template <typename T>
+int LinkedList<T>::getLength() const {
+	if (head == nullptr) {
+		return 0;
+	}
+	if (head->m_Next == nullptr) {
+		return 1;
+	}
+	auto len{ 0 };
+	auto curr = head;
+
+	while (curr != nullptr) {
+		len++;
+		curr = curr->m_Next;
+	}
+
+	return len;
+}
+
+template <typename T>
+void LinkedList<T>::deleteNodeStart() {
+	if (head == nullptr) {
+		std::cout << "Can't delete, your Linked List is empty" << '\n';
+		return;
+	}
+	if (head->m_Next == nullptr) {
+		head = nullptr;
+		return;
+	}
+
+	auto temp = head;
+	head = head->m_Next;
+	temp->m_Next = nullptr;
+}
+
+template <typename T>
+void LinkedList<T>::deleteNodeEnd() {
+	if (head == nullptr) {
+		std::cout << "Can't delete. Your Linked List is empty" << '\n';
+		return;
+	}
+	if (head->m_Next == nullptr) {
+		head = nullptr;
+		return;
+	}
+
+	auto curr = head;
+	auto prev = curr;
+
+	while (curr->m_Next != nullptr) {
+		prev = curr;
+		curr = curr->m_Next;
+	}
+
+	prev->m_Next = nullptr;
+}
+
+template <typename T>
+void LinkedList<T>::deleteNodePos(unsigned int pos) {
+	if (head == nullptr) {
+		std::cout << "Can't delete. Your Linked List is empty" << '\n';
+		return;
+	}
+	if (head->m_Next == nullptr && pos == 1) {
+		head = nullptr;
+		return;
+	}
+
+	if (pos > getLength()) {
+		std::cout << "Can't delete. Entered position is out of range. Your linked list is of length: " << getLength() << '\n';
+		return;
+	}
+
+	if (pos == 1) {
+		head = head->m_Next;
+		return;
+	}
+
+	auto cnt{ 1 };
+	auto curr = head;
+	auto prev = head;
+
+	while (cnt != pos) {
+		prev = curr;
+		curr = curr->m_Next;
+		cnt++;
+	}
+
+	prev->m_Next = curr->m_Next;
+	curr->m_Next = nullptr;
+}
+
+template <typename T>
+void LinkedList<T>::displayList() {
+	if (head == nullptr) {
+		std::cout << "Can't display. Your linked list is empty" << '\n';
+		return;
+	}
+
+	auto temp = head;
+
+	while (temp->m_Next != nullptr) {
+		std::cout << temp->m_Data << "->";
+		temp = temp->m_Next;
+	}
+	std::cout << temp->m_Data << '\n';
+}
 
 #endif
--- Assignment-3/main.cpp
+++ Assignment-3/main.cpp
@@ -3,24 +3,63 @@
 
 int main()
 {
-    LinkedList l1{20, 30};
+    LinkedList<int> l1{ 20, 30 };
     l1.displayList();
+    std::cout << l1.getLength() << '\n';
     
     l1.insertNodeStart(10);
     l1.displayList();
+    std::cout << l1.getLength() << '\n';
     
     l1.insertNodeEnd(50);
     l1.displayList();
+    std::cout << l1.getLength() << '\n';
 
     l1.insertNodePos(4, 40);
     l1.displayList();
+    std::cout << l1.getLength() << '\n';
 
     l1.deleteNodeStart();
     l1.displayList();
+    std::cout << l1.getLength() << '\n';
 
     l1.deleteNodeEnd();
     l1.displayList();
+    std::cout << l1.getLength() << '\n';
 
     l1.deleteNodePos(2);
     l1.displayList();
+    std::cout << l1.getLength() << '\n';
+
+    std::cout << "----------------------" << '\n';
+    LinkedList<float> l2{ 1.5, 3.0};
+    l2.displayList();
+
+    l2.insertNodeStart(0.05);
+    l2.displayList();
+
+    l2.deleteNodeEnd();
+    l2.displayList();
+
+    l2.deleteNodeStart();
+    l2.displayList();
+
+    l2.deleteNodePos(1);
+    l2.displayList();
+
+    std::cout << "----------------------" << '\n';
+    LinkedList <std::string> l3{ "abc", "def" };
+    l3.displayList();
+
+    l3.insertNodeStart("nhd");
+    l3.displayList();
+
+    l3.deleteNodeEnd();
+    l3.displayList();
+
+    l3.deleteNodeStart();
+    l3.displayList();
+
+    l3.deleteNodePos(1);
+    l3.displayList();
 }
